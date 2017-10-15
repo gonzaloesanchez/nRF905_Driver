6bb3234b098e26e37d50a55169a7e805694d3035
El_Gonza <gonzalo.e.sanchez@hotmail.com>
2017-10-14 21:46:01 -0300

    Primeras definciones de HW
    Funciones de HAL implementadas

---
+++ inc/Inicializacion.h
@@ -0,0 +1,22 @@
+/*
+ * Inicializacion.h
+ *
+ *  Created on: 06/01/2014
+ *      Author: Gonza
+ */
+
+#ifndef INICIALIZACION_H_
+#define INICIALIZACION_H_
+
+void ConfigWDT(void);
+void FaultRoutine(void);
+void ConfigClocks(void);
+void ConfigTimer0_A3(void);
+void ConfigTimer1_A3(void);
+void ConfigPorts(void);
+void ConfigUART(void);
+void ConfigCompA(void);
+void ConfigUSCIB0(void);
+void	ConfigADC10(void);
+
+#endif /* INICIALIZACION_H_ */
--- inc/main.h
+++ inc/main.h
@@ -7,45 +7,6 @@
 
 #ifndef NRF905_DRIVER_INC_MAIN_H_
 #define NRF905_DRIVER_INC_MAIN_H_
-
-/* Copyright 2014, 2015 Mariano Cerdeiro
- * Copyright 2014, Pablo Ridolfi
- * Copyright 2014, Juan Cecconi
- * Copyright 2014, Gustavo Muro
- * All rights reserved.
- *
- * This file is part of CIAA Firmware.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
-
-#ifndef MAIN_H
-#define MAIN_H
 /** \brief Bare Metal example header file
  **
  ** This is a mini example of the CIAA Firmware
@@ -170,8 +131,5 @@ extern void _vStackTop(void);
 /** @} doxygen end group definition */
 /** @} doxygen end group definition */
 /*==================[end of file]============================================*/
-#endif /* #ifndef BAREMETAL_H */
-
-
 
 #endif /* NRF905_DRIVER_INC_MAIN_H_ */
